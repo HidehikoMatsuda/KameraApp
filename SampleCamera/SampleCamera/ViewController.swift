7a759ace49e10d501102e444de3928796e52e075
hidehiko <hxbby24develop@gmail.com>
2019-09-05 09:36:04 +0900

    1st commit

---
+++ SampleCamera/SampleCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleCamera
+//
+//  Created by Hidehiko Matsuda on 2019/09/05.
+//  Copyright © 2019 Hidehiko Matsuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
